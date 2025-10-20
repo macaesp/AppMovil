41dfc10ec7386cbec1c42ebc0c85b5dacc327d1e
Cely <ara.escobar@duocuc.cl>
2025-10-20 20:01:19 -0300

    Cambios en ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ android {
 dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation(libs.androidx.compose.runtime.livedata)
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0-rc02")
