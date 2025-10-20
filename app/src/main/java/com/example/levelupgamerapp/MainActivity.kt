7f87d8966f7f75c9be374e027b88df669fbc4fbe
macaesp <mac.espinoza@duocuc.cl>
2025-10-20 17:06:04 -0300

    Arreglo del ui-theme

--- app/src/main/java/com/example/levelupgamerapp/MainActivity.kt
+++ app/src/main/java/com/example/levelupgamerapp/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.levelupgamerapp.ui.theme.LevelupGamerAppTheme
+import com.example.levelupgamerapp.ui.theme.LevelUpGamerAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LevelupGamerAppTheme {
+            LevelUpGamerAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    LevelupGamerAppTheme {
+    LevelUpGamerAppTheme {
         Greeting("Android")
     }
 }
--- app/src/main/java/com/example/levelupgamerapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/levelupgamerapp/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -34,13 +35,14 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun LevelupGamerAppTheme(
+fun LevelUpGamerAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
-) {
-    val colorScheme = when {
+
+){
+    val colorScheme = when{
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
@@ -48,11 +50,12 @@ fun LevelupGamerAppTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
 
+    }
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
 }
+
