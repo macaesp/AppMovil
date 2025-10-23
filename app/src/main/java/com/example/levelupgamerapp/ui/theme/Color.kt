774c8297a42d1994c11b7926fa75e2b0b386fda6
Benja <bi.gonzalezr@duocuc.cl>
2025-10-23 18:01:24 -0300

    actualizado theme

--- app/src/main/java/com/example/levelupgamerapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/levelupgamerapp/ui/theme/Color.kt
@@ -2,8 +2,13 @@ package com.example.levelupgamerapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val NeonGreen = Color(0xFF76FF03)
+
 val DarkBackground = Color(0xFF111111)
 val DarkSurface = Color(0xFF1A1A1A)
 val LightGray = Color(0xFFBBBBBB)
 val PureWhite = Color(0xFFFFFFFF)
+val NeonGreen = Color(0xFF76FF03)
+
+val LightBackground = Color(0xFFFFFFFF)
+val LightGrayText = Color(0xFFB0B0B0)
+val ErrorRed = Color(0xFFFF5252)
